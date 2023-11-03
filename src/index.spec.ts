e080ff3eba103b234b0915c5274de3f4b15b7e2f
Eóin Martin <eoinmartin3007@gmail.com>
2023-11-03 16:05:53 +1100

    test(packer): add tests for printOutput and thrown errors

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -88,49 +88,51 @@ describe('Packer', () => {
 			const expectedOutput = { index: 1, weight: 53.38, price: 45 };
 			expect(packer._parseItem(item)).toEqual(expectedOutput);
 		});
+
+		it('should throw error if weight of item is over 100', () => {
+			const packer = new Packer();
+			const item = '(1,153.38,€45)';
+			expect(() => {
+				packer._parseItem(item);
+			}).toThrow(`Item weight exceeds maximum`);
+		});
+
+		it('should throw error if price of item is over 100', () => {
+			const packer = new Packer();
+			const item = '(1,53.38,€145)';
+			expect(() => {
+				packer._parseItem(item);
+			}).toThrow(`Item price exceeds maximum`);
+		});
 	});
 
-	xdescribe('_printOutput', () => {
+	describe('_printOutput', () => {
 		it('should return the correct string containing indexes of items in each package', () => {
 			const packer = new Packer();
 			packer.packages = [
 				{
 					value: 81,
-					items: [
-						{ index: 1, weight: 53.38, price: 45 },
-						{ index: 2, weight: 88.62, price: 98 },
-						{ index: 3, weight: 78.48, price: 3 },
-					],
+					items: [{ index: 2, weight: 88.62, price: 98 }],
 				},
-				{ value: 8, items: [{ index: 1, weight: 15.3, price: 34 }] },
+				{ value: 0, items: [] },
 				{
 					value: 75,
 					items: [
 						{ index: 1, weight: 85.31, price: 29 },
-						{ index: 2, weight: 14.55, price: 74 },
 						{ index: 3, weight: 3.98, price: 16 },
-						{ index: 4, weight: 26.24, price: 55 },
 						{ index: 5, weight: 63.69, price: 52 },
-						{ index: 6, weight: 76.25, price: 75 },
-						{ index: 7, weight: 60.02, price: 74 },
-						{ index: 8, weight: 93.18, price: 35 },
 					],
 				},
 				{
 					value: 56,
 					items: [
 						{ index: 1, weight: 90.72, price: 13 },
-						{ index: 2, weight: 33.8, price: 40 },
 						{ index: 3, weight: 43.15, price: 10 },
-						{ index: 4, weight: 37.97, price: 16 },
-						{ index: 5, weight: 46.81, price: 36 },
 						{ index: 6, weight: 48.77, price: 79 },
-						{ index: 7, weight: 81.8, price: 45 },
-						{ index: 8, weight: 19.36, price: 79 },
 					],
 				},
 			];
-			const expectedOutput = '4\n-\n2,7\n8,9';
+			const expectedOutput = '2\n-\n1,3,5\n1,3,6';
 			expect(packer._printOutput()).toEqual(expectedOutput);
 		});
 	});
